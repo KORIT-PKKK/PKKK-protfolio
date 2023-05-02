6bf85ed7ceb64632be96c9cb22064462e35a22e9
0476a <0476a@naver.com>
2023-05-02 14:31:06 +0900

    init: 프로젝트 초기 세팅

---
+++ pkkk/src/main/java/com/portfolio/pkkk/pkkk/PkkkApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.pkkk.pkkk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PkkkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PkkkApplication.class, args);
+	}
+
+}
---
+++ pkkk/src/test/java/com/portfolio/pkkk/pkkk/PkkkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.pkkk.pkkk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PkkkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
