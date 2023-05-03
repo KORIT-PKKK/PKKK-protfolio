df580067dd2fe520c6dd8bf633cb1549f1a80a97
eunbinpark <534311@gmail.com>
2023-05-03 17:49:36 +0900

    2023.05.03 app.js에 피드 경로 추가

--- fornt_pkkk/src/App.js
+++ fornt_pkkk/src/App.js
@@ -4,6 +4,7 @@ import { Reset } from './styles/Global/reset';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/home/Home';
 import Visit from './pages/visit/Visit';
+import Feed from './pages/feed/Feed';
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />}></Route>
         <Route path='/visit' element={<Visit />}></Route>
+        <Route path='/feed' element={<Feed />}></Route>
       </Routes>
     </>
   );
