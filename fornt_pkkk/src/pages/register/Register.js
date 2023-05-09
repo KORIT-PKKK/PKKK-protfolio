c8af74cf23b5e872dd5f36d11d0e65ad4183dc3c
0476a <0476a@naver.com>
2023-05-09 09:43:57 +0900

    Feat: 회원가입 추가

--- fornt_pkkk/src/pages/register/Register.js
+++ fornt_pkkk/src/pages/register/Register.js
@@ -4,9 +4,25 @@ import React from 'react';
 
 const register = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <header css={S.header}>
+                <div css={S.compony}>PKKK 플레이스</div>
+            </header>
+            <div css={S.inputBox}>
+                <h3 css={S.inputTitle}>아이디</h3>
+                <input type="text" css={S.input}/>
+                <div css={S.errorMessage}>5~20자의 영문 소문자, 숫자와 특수기호(_),(-)만 사용 가능합니다.</div>
+                <h3 css={S.inputTitle}>비밀번호</h3>
+                <input type="password" css={S.input}/>
+                <div css={S.errorMessage}>8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.</div>
+                <h3 css={S.inputTitle}>이름</h3>
+                <input type="text" css={S.input}/>
+                <div css={S.errorMessage}>필수 정보입니다.</div>
+            </div>
+            <div css={S.joinButtonBox}>
+                <button css={S.joinButton}>가입하기</button>
+            </div>
+        </>
     );
 };
 
--- fornt_pkkk/src/pages/register/style/RegisterStyle.js
+++ fornt_pkkk/src/pages/register/style/RegisterStyle.js
@@ -1,1 +1,46 @@
 const { css } = require("@emotion/react");
+
+export const header = css`
+    width: 768px;
+    height: 124px;
+    margin: 0px auto;
+    padding: 60px 0px 20px;
+`;
+
+export const compony = css`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 44px;
+    font-weight: 600;
+`;
+
+export const inputBox = css`
+    width: 460px;
+    margin: 0px auto;
+`;
+
+export const inputTitle = css`
+    margin: 19px 0px 8px;
+    font-size: 14px;
+`;
+
+export const input = css`
+    width: 100%;
+    height: 51px;
+    padding: 10px 14px;
+`;
+
+export const errorMessage = css`
+    margin: 9px 0px -2px;
+    font-size: 12px;
+    color: #ff0000;
+`;
+
+export const joinButtonBox = css`
+    
+`;
+
+export const joinButton = css`
+    
+`;
