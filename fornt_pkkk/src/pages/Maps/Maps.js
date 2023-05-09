e4076aac5fffbbfc940563e5e6347d2eb0fd16b2
eunbinpark <534311@gmail.com>
2023-05-09 15:23:36 +0900

    2023.05.09 / 맵 내 주변 위치로 변경

--- fornt_pkkk/src/pages/Maps/Maps.js
+++ fornt_pkkk/src/pages/Maps/Maps.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
 
 const containerStyle = {
@@ -6,28 +6,40 @@ const containerStyle = {
   height: '400px'
 };
 
-const center = {
-  lat: -3.745,
-  lng: -38.523
-};
-
 const Maps = () => {
     const { isLoaded } = useJsApiLoader({
         id: 'google-map-script',
-        googleMapsApiKey: "YOUR_API_KEY"
+        googleMapsApiKey: "key"
       })
     
-      const [map, setMap] = React.useState(null)
-    
-      const onLoad = React.useCallback(function callback(map) {
+    useEffect(() => {
+        if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+            setCenter({
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            });
+            },
+            (error) => {
+            console.error(error);
+            }
+        );
+        }
+    }, []);
+
+    const [map, setMap] = React.useState(null)
+    const [center, setCenter] = useState({ lat: 0, lng: 0 });
+
+    const onLoad = React.useCallback(function callback(map) {
         const bounds = new window.google.maps.LatLngBounds(center);
         map.fitBounds(bounds);
         setMap(map)
-      }, [])
+    }, [center]);
     
-      const onUnmount = React.useCallback(function callback(map) {
+    const onUnmount = React.useCallback(function callback(map) {
         setMap(null)
-      }, [])
+    }, [])
     
       return isLoaded ? (
           <GoogleMap
