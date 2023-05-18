bc5f85c420a8e3e4b533fa623d3295266b511a3e
eunbinpark <534311@gmail.com>
2023-05-18 15:04:11 +0900

    Feat: 구글 플레이스 템플릿 수정중

--- fornt_pkkk/src/pages/google/GooglePlace.js
+++ fornt_pkkk/src/pages/google/GooglePlace.js
@@ -1,9 +1,48 @@
-import React from 'react';
+import { GoogleMap, InfoWindow, Marker } from '@react-google-maps/api';
+import React, { useState } from 'react';
 
 const GooglePlace = () => {
+  const [selectedPlace, setSelectedPlace] = useState(null);
+
+  const handleMarkerClick = (place) => {
+    setSelectedPlace(place);
+  };
+
+  const handleInfoWindowClose = () => {
+    setSelectedPlace(null);
+  };
+
   return (
     <div>
-
+      <GoogleMap
+      defaultZoom={16}
+      defaultCenter={{ lat: 35.1595, lng: 129.0599 }}
+    >
+      <Marker
+        position={{ lat: 35.1595, lng: 129.0599 }}
+        onClick={() =>
+          handleMarkerClick({
+            name: '삼정타워',
+            address: '부산광역시 부산진구 양정동',
+            phone: '010-1234-5678',
+            businessHours: '09:00 AM - 06:00 PM'
+          })
+        }
+      />
+      {selectedPlace && (
+        <InfoWindow
+          position={{ lat: 35.1595, lng: 129.0599 }}
+          onCloseClick={handleInfoWindowClose}
+        >
+          <div>
+            <h2>{selectedPlace.name}</h2>
+            <p>{selectedPlace.address}</p>
+            <p>전화번호: {selectedPlace.phone}</p>
+            <p>영업 시간: {selectedPlace.businessHours}</p>
+          </div>
+        </InfoWindow>
+      )}
+    </GoogleMap>
     </div>
   );
 };
